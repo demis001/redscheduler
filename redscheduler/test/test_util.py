f46a5df22b9d085aef9d8eba4b51d0d08e2b5c58
Tyghe Vallard <vallardt@gmail.com>
2015-02-26 11:20:23 -0500

    More utility functions for system info

--- redscheduler/test/test_util.py
+++ redscheduler/test/test_util.py
@@ -2,14 +2,28 @@ from . import unittest, mock, CONFIG_EXAMPLE, builtins
 
 from .. import util
 
+sysinfo = {
+    'meminfo': [
+        'MemTotal:\t10000000 kB', # 10G, 10000M
+        'MemFree:\t4000000 kB',
+        'Buffers:\t1000000 kB',
+        'Cached:\t4000000 kB'
+    ],
+    'cpuinfo': [
+        'processor   : 0',
+        'cpu MHz     : 1400.000',
+        'cache size  : 2048 KB',
+        'physical id : 0',
+        'siblings    : 6',
+        'core id     : 0',
+        'cpu cores   : 3',
+    ],
+    'loadavg': ['0.01', '0.10', '1.00', '5/100', '1']
+}
+
 class TestParseMemInfo(unittest.TestCase):
     def setUp(self):
-        self.meminfo = [
-            'MemTotal:\t10000000 kB',
-            'MemFree:\t4000000 kB',
-            'Buffers:\t1000000 kB',
-            'Cached:\t5000000 kB'
-        ]
+        self.meminfo = sysinfo['meminfo']
         self.patch_open = mock.patch('redscheduler.util.open')
         self.mock_open = self.patch_open.start()
         self.addCleanup(self.patch_open.stop)
@@ -25,19 +39,11 @@ class TestParseMemInfo(unittest.TestCase):
         self.assertEqual('10000000 kB', r['MemTotal'])
         self.assertEqual('4000000 kB', r['MemFree'])
         self.assertEqual('1000000 kB', r['Buffers'])
-        self.assertEqual('5000000 kB', r['Cached'])
+        self.assertEqual('4000000 kB', r['Cached'])
 
-class TestParseCpuInfo(unittest.TestCase):
+class TestGetCpuInfo(unittest.TestCase):
     def setUp(self):
-        self.cpuinfo = [
-            'processor   : 0',
-            'cpu MHz     : 1400.000',
-            'cache size  : 2048 KB',
-            'physical id : 0',
-            'siblings    : 6',
-            'core id     : 0',
-            'cpu cores   : 3',
-        ]
+        self.cpuinfo = sysinfo['cpuinfo']
         self.patch_open = mock.patch('redscheduler.util.open')
         self.mock_open = self.patch_open.start()
         self.addCleanup(self.patch_open.stop)
@@ -54,7 +60,7 @@ class TestParseCpuInfo(unittest.TestCase):
         self.assertIn('processor', r[0])
         self.assertIn('processor', r[1])
 
-class TestParseColonTab(unittest.TestCase):
+class TestGetColonTab(unittest.TestCase):
     def test_parses_returns_dictionary(self):
         colontab = [
             'foo1:bar',
@@ -67,3 +73,50 @@ class TestParseColonTab(unittest.TestCase):
         self.assertEqual('bar', r['foo2'])
         self.assertEqual('bar', r['foo3'])
         self.assertEqual('bar baz', r['foo4'])
+
+class TestGetLoadavg(unittest.TestCase):
+    def setUp(self):
+        self.loadavg = sysinfo['loadavg']
+        self.patch_open = mock.patch('redscheduler.util.open')
+        self.mock_open = self.patch_open.start()
+        self.addCleanup(self.patch_open.stop)
+
+    def test_opens_proc_meminfo(self):
+        util.get_loadavg()
+        self.mock_open.assert_called_with('/proc/loadavg')
+
+    def test_builds_dictionary_correctly(self):
+        contents = '\t'.join(self.loadavg)
+        self.mock_open.return_value.__enter__.return_value.read.return_value = contents
+        r = util.get_loadavg()
+        self.assertEqual(0.01, r['1min'])
+        self.assertEqual(0.1, r['5min'])
+        self.assertEqual(1.0, r['15min'])
+        self.assertEqual('5/100', r['procs'])
+        self.assertEqual('1', r['lastpid'])
+
+class TestGetAvailmem(unittest.TestCase):
+    def setUp(self):
+        self.meminfo = util.parse_colon_tab('\n'.join(sysinfo['meminfo']))
+
+    def test_returns_kilobytes(self):
+        r = util.get_availmem(self.meminfo, 'K')
+        self.assertEqual(9000000, r)
+
+    def test_returns_megabytes(self):
+        r = util.get_availmem(self.meminfo, 'M')
+        self.assertEqual(9000, r)
+
+    def test_returns_gigabytes(self):
+        r = util.get_availmem(self.meminfo, 'G')
+        self.assertEqual(9, r)
+
+    def test_uneven_division(self):
+        self.meminfo['MemFree'] = '3333333 kB'
+        self.meminfo['Buffers'] = '1111111 kB'
+        self.meminfo['Cached'] =  '1111111 kB'
+        r = util.get_availmem(self.meminfo, 'M')
+        self.assertEqual(5555, r)
+
+    def test_invalid_unit_raises_exception(self):
+        self.assertRaises(ValueError, util.get_availmem, self.meminfo, 'f')
--- redscheduler/util.py
+++ redscheduler/util.py
@@ -32,7 +32,7 @@ def get_mem_info():
 def get_cpu_info():
     '''
     Read /proc/cpuinfo and return parsed results
-    :return: dict of cpuinfo results
+    :return: list of dict of cpuinfo results
     '''
     cpus = []
     with open('/proc/cpuinfo') as fh:
@@ -40,3 +40,40 @@ def get_cpu_info():
         for cpu in contents.split('\n\n'):
             cpus.append(parse_colon_tab(cpu))
     return cpus
+
+def get_loadavg():
+    '''
+    Read /proc/loadavg and return as a dict of
+    {'1min', '5min', '15min', 'procs', 'lastpid'} 
+
+    :return: dict of meminfo results
+    '''
+    loadavg = {}
+    with open('/proc/loadavg') as fh:
+        parts = fh.read().strip().split()
+        loadavg['1min'] = float(parts[0])
+        loadavg['5min'] = float(parts[1])
+        loadavg['15min'] = float(parts[2])
+        loadavg['procs'] = parts[3]
+        loadavg['lastpid'] = parts[4]
+    return loadavg
+
+def get_availmem(meminfo, unit='K'):
+    '''
+    Return the ammount of memory that is really free
+    MemFree + Buffers + Cached
+
+    :param dict meminfo: get_mem_info dictionary
+    :param str unit: Unit to return free memory in valid values ('K','M',G)
+    :return: int of how much free memory(in units)
+    '''
+    unit_map = {
+        'K': 1,
+        'M': 1000,
+        'G': 1000000
+    }
+    if unit not in unit_map:
+        raise ValueError('{0} is not a valid unit identifier'.format(unit))
+    avail = [meminfo['MemFree'], meminfo['Buffers'], meminfo['Cached']]
+    avail = sum(map(lambda x: int(x.split()[0]), avail))
+    return avail / unit_map[unit]
